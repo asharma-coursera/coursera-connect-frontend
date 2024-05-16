4d0e26a9925a045fa853a72c52f5790d6e6682ac
asharma <asharma@coursera.org>
2024-05-16 15:47:32 +0530

    add recruiter home page and add job functionality

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import reactLogo from "./assets/react.svg";
 import {Button, TextField} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 const HomePage: React.FC = () => {
+
+    const [email, setEmail] = useState('');
+    const history = useNavigate();
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleLoginClick = () => {
+        history( '/empty', // Path of the page you want to navigate to
+             {
+        state: {
+            email: email
+        } }) // State object containing email);
+    };
+
     return (
         <>
             <div>
@@ -12,10 +29,11 @@ const HomePage: React.FC = () => {
             </div>
             <h1>Coursera connect</h1>
             <div>
-                <TextField id="outlined-basic" label="Email" variant="outlined" />
+                <TextField id="outlined-basic" label="Email" variant="outlined" value={email} // Set the value of TextField to the email state variable
+                           onChange={handleEmailChange} />
             </div>
             <br />
-            <Button variant="contained">Log in</Button>
+            <Button variant="contained" onClick={handleLoginClick}>Log in</Button>
         </>
     );
 };
--- src/recruiterHomePage.tsx
+++ src/recruiterHomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,6 +6,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import {useLocation} from "react-router-dom";
+import {Box, Button, Modal, TextField} from "@mui/material";
 
 function createData(
     name: string,
@@ -14,12 +16,49 @@ function createData(
     return { name, description };
 }
 
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 const rows = [
     createData('SDE 1', 'description for SDE1'),
     createData('SDE 2', 'description for SDE2'),
 ];
 
 const BasicTable = ()=> {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://catfact.ninja/fact');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -27,18 +66,29 @@ const BasicTable = ()=> {
                     <TableRow>
                         <TableCell>Job</TableCell>
                         <TableCell align="right">Description</TableCell>
+                        <TableCell align="right"></TableCell>
+                        <TableCell align="right"></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
+                        <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                             <TableCell component="th" scope="row">
                                 {row.name}
                             </TableCell>
-                            <TableCell align="right">{row.description}</TableCell>
+                            <TableCell align="right">
+                                {row.description}
+                            </TableCell>
+                            <TableCell align="right">
+                                <Button onClick={() => {}} variant="contained" color="primary">
+                                    View details
+                                </Button>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Button onClick={() => {}} variant="contained" color="primary">
+                                    Search users
+                                </Button>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
@@ -47,11 +97,46 @@ const BasicTable = ()=> {
     );
 }
 
-const RecruiterHomePage: React.FC = () => {
+const RecruiterHomePage: React.FC = (props: { email: string}) => {
+    const location = useLocation();
+    const { email } = location.state || {};
+    const [job, setJob] = useState('');
+    const [description, setDescription] = useState('');
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => {
+        setOpen(false);
+        rows.push({ name: job, description })
+    };
+    const handleJobChange = (event) => {
+        setJob(event.target.value);
+    };
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
     return (
         <div>
             <h1>This is a Recruiter's home page</h1>
             <BasicTable></BasicTable>
+            <br/>
+            <Button onClick={handleOpen}>Add Job</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <TextField id="outlined-basic" label="job" variant="outlined" value={job}
+                               onChange={handleJobChange}/>
+                    <br/>
+                    <br/>
+                    <TextField id="outlined-basic" label="description" variant="outlined" value={description}
+                               onChange={handleDescriptionChange}/>
+                    <br/>
+                    <Button onClick={handleClose}>Add job</Button>
+                </Box>
+            </Modal>
         </div>
     );
 };
